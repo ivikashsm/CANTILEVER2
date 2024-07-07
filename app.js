6a8598f01648aab676c68b4056a4723fbc97e587
Vikash Singh Munda <vikashsinghmunda2000@gmail.com>
2024-07-07 07:09:39 +0530

    7-july

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
+
 const db = require("./config/mongoose-connection");
 
 // Middleware
@@ -33,11 +37,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 // Routes
-app.get("/", (req, res) => {
-    res.send("hey");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000);
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch1")
+.connect("mongodb://127.0.0.1:27017/NewsApp")
 .then(function(){
     console.log("connected");
 })
