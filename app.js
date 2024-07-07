7aaeea0fc77164a50a7d8cefa642c7335757d119
Vikash Singh Munda <vikashsinghmunda2000@gmail.com>
2024-07-07 06:09:41 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,43 @@
+// const express = require("express");
+// const app = express();
+// const cookieParser = require("cookie-parser");
+// const path = require("path");
+
+// const db = require("./config/mongoose-connection");
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname,"public")));
+// app.set("view engine","ejs");
+
+// app.get("/",(req,res)=>{
+//     res.send("hey");
+// });
+
+
+// app.listen(3000);
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const db = require("./config/mongoose-connection");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Corrected this line
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("hey");
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
